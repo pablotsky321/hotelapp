142996597431971a27598385c534a42df2d2e45f
JUAN PABLO RESTREPO JIMENEZ <jprestrepo346@gmail.com>
2024-07-19 11:58:41 -0500

    removed unused import

--- microservice-user/src/main/java/com/service/microservice_user/repositories/UserRepository.java
+++ microservice-user/src/main/java/com/service/microservice_user/repositories/UserRepository.java
@@ -2,7 +2,6 @@ package com.service.microservice_user.repositories;
 
 import com.service.microservice_user.entities.UserEntity;
 import jakarta.transaction.Transactional;
-import org.hibernate.annotations.SQLDelete;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
